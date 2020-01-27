f29cb0f81582125fbc19ef21ec06034c107ba92a
amirgal <amirc538@gmail.com>
2020-01-27 18:49:09 +0200

    added frog icon and size change by y axis

--- renderer.js
+++ renderer.js
@@ -4,10 +4,11 @@ const Renderer = () => {
         const container = $('#container')
         
         for(let frog of frogs) {
-            const frogElem = $(`<div class="frog" data-id=${frog.id}></div>`)
-            frogElem.css('background-color',`${frog.color}`)
+            const frogElem = $(`<i class="fas fa-frog frog" data-id=${frog.id}></i>`)
+            frogElem.css('color',`${frog.color}`)
             frogElem.css('top',`${frog.y}px`)
             frogElem.css('left',`${frog.x}px`)
+            frogElem.css('font-size',`${frog.y/5}px`)
             container.append(frogElem)
         }
     }

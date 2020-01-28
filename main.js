420ea28063810f656beba195bcbd02c06cdac3af
amirgal <amirc538@gmail.com>
2020-01-28 22:54:28 +0200

    fixed bug on the click frog function

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const game = Game()
 const renderer = Renderer()
+let counter = 0
 
 $('#startBtn').on('click', function () {
     game.levelUp()
     renderer.renderFrogs(game.getFrogs())
     renderer.renderLevel(game.getLevel())
     $('#startBtn').attr('disabled',true)
-    startCountDown(game.getLevel()+1)
+    counter = game.getLevel()+1
+    startCountDown(counter)
 })
 
 $('#container').on('click','.frog', function() {
     if(1 == game.getFrogs().length){
-        const frogID = $(this).data().id
-        game.removeFrog(frogID)
+        game.removeFrog($(this).data().id)
         game.levelUp()
         renderer.renderFrogs(game.getFrogs())
         renderer.renderLevel(game.getLevel())
         startCountDown(-1)
-        startCountDown(game.getLevel()+1)
+        counter = game.getLevel()+1
+        startCountDown(counter)
+    } else {  
+        game.removeFrog($(this).data().id)
+        renderer.renderFrogs(game.getFrogs())
     }
-    const frogID = $(this).data().id
-    game.removeFrog(frogID)
-    renderer.renderFrogs(game.getFrogs())
 })
 
 const startCountDown = function(counter) {
--- renderer.js
+++ renderer.js
@@ -5,12 +5,12 @@ const Renderer = () => {
         const container = $('#container')
         
         for(let frog of frogs) {
-            const frogElem = $(`<i class="fas fa-frog frog" data-id=${frog.id}></i>`)
-            frogElem.css('color',`${frog.color}`)
-            frogElem.css('top',`${frog.y}px`)
-            frogElem.css('left',`${frog.x}px`)
-            frogElem.css('font-size',`${frog.y/4}px`)
-            container.append(frogElem)
+            const newFrog = $(`<i class="fas fa-frog frog" data-id=${frog.id}></i>`)
+            newFrog.css('color',`${frog.color}`)
+            newFrog.css('top',`${frog.y}px`)
+            newFrog.css('left',`${frog.x}px`)
+            newFrog.css('font-size',`${frog.y/4}px`)
+            container.append(newFrog)
         }
         $('#frogs-left').append(`<p>${frogs.length} Frogs Left<p>`)
         

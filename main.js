b3d0b7d8081220287452ac0a9926df6921c01246
amirgal <amirc538@gmail.com>
2020-01-29 20:45:26 +0200

    TODO: game ends a second after 0 and doesnt show game over

--- main.js
+++ main.js
@@ -10,36 +10,36 @@ $('#startBtn').on('click', function () {
     counter = game.getLevel()+1
     
     $('#time').text(counter)
-    const timer = setInterval(timeFunc,1000)
     
-})
-
-const timeFunc = function() {
-    counter--
-    if(counter < 0) {
-        clearInterval(timer)
-        $('#container').append(`<h1 id="game-over">Game Over!<h1>`)
-        return
-    } else {
-        $('#time').text(counter)
+    const timeFunc = function() {
+        counter--
+        if(counter < 0) {
+            clearInterval(timer)
+            $('#container').append(`<h1 id="game-over">Game Over!<h1>`)
+            renderer.renderFrogs([])
+            return
+        } else {
+            $('#time').text(counter)
+        }
+        
     }
-}
+    const timer = setInterval(timeFunc,1000)
 
-$('#container').on('click','.frog', function() {
-    if(1 == game.getFrogs().length){
-        game.removeFrog($(this).data().id)
-        game.levelUp()
-        renderer.renderFrogs(game.getFrogs())
-        renderer.renderLevel(game.getLevel())
-        counter = game.getLevel()+1
-        timeFunc()
-    } else {  
-        game.removeFrog($(this).data().id)
-        renderer.renderFrogs(game.getFrogs())
-    }
+    $('#container').on('click','.frog', function() {
+        if(1 == game.getFrogs().length){
+            game.removeFrog($(this).data().id)
+            game.levelUp()
+            renderer.renderFrogs(game.getFrogs())
+            renderer.renderLevel(game.getLevel())
+            counter = game.getLevel()+1
+            timeFunc()
+        } else {  
+            game.removeFrog($(this).data().id)
+            renderer.renderFrogs(game.getFrogs())
+        }
+    })
+
+
 })
-
-
-
 
 

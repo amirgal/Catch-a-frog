b8c5cfcf3b198540765ecedb53a8f1d09bcda349
amirgal <amirc538@gmail.com>
2020-01-29 20:09:18 +0200

    added timeFunc and called it on last frog click

--- main.js
+++ main.js
@@ -10,18 +10,20 @@ $('#startBtn').on('click', function () {
     counter = game.getLevel()+1
     
     $('#time').text(counter)
-    const timer = setInterval(function() {
-        counter--
-        if(counter < 0) {
-            clearInterval(timer)
-            $('#container').append(`<h1 id="game-over">Game Over!<h1>`)
-            return
-        } else {
-            $('#time').text(counter)
-        }
-    },1000)
+    const timer = setInterval(timeFunc,1000)
     
 })
+
+const timeFunc = function() {
+    counter--
+    if(counter < 0) {
+        clearInterval(timer)
+        $('#container').append(`<h1 id="game-over">Game Over!<h1>`)
+        return
+    } else {
+        $('#time').text(counter)
+    }
+}
 
 $('#container').on('click','.frog', function() {
     if(1 == game.getFrogs().length){
@@ -30,7 +32,7 @@ $('#container').on('click','.frog', function() {
         renderer.renderFrogs(game.getFrogs())
         renderer.renderLevel(game.getLevel())
         counter = game.getLevel()+1
-
+        timeFunc()
     } else {  
         game.removeFrog($(this).data().id)
         renderer.renderFrogs(game.getFrogs())
